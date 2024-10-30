acc519c878ae2b5ffb5dd3a78e3327a690e02117
chaudhryu <135050470+chaudhryu@users.noreply.github.com>
2024-10-30 08:02:52 -0700

    adding pics

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -5,7 +5,6 @@ const DefaultLayout = () => {
   return (
     <div>
       <div className="wrapper d-flex flex-column min-vh-100">
-        <AppHeader />
         <div className="body flex-grow-1">
           <AppContent />
         </div>
