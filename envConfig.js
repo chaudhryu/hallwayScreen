0e8c582ebcc5aad6dbb5b5eb213cb946cb2c043d
chaudhryu <135050470+chaudhryu@users.noreply.github.com>
2024-11-05 11:54:46 -0800

    Updated with UI fixes

---
+++ envConfig.js
@@ -0,0 +1,13 @@
+// src/envConfig.js
+
+const config = {
+    CONDECO_AUTH_API:'https://metro.condecosoftware.com/tokenproviderapi/token',
+    CONDECO_BOOKINGS_API:'https://integrationapi.condecosoftware.com/Los_Angeles_County/api/V1/bookings',
+    CONDECO_PASSWORD:'NTIyM2NkNzhjY2E1NDgxYWIyYjA4OGU3NGE1ZGNiY2U=',
+    CONDECO_CLIENT_ID:'5223cd78cca5481ab2b088e74a5dcbce',
+    CONDECO_SUBSCRIPTION_KEY:'e9dfa515163c409bbc5cf5f38d004490',
+    FIFTH_FLOOR_ROOM_ID:'143', // Replace with your actual room ID
+  };
+  
+  export default config;
+  
