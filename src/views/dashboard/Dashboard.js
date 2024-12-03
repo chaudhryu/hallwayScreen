cb778e7c926f297b3e0d0779200df21f12dafa5d
chaudhryu <135050470+chaudhryu@users.noreply.github.com>
2024-12-03 08:13:21 -0800

    Update Dashboard.js

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,5 +1,3 @@
-// Dashboard.js
-
 import React, { useState, useEffect } from 'react';
 import {
   CCard,
@@ -8,7 +6,6 @@ import {
   CCardTitle,
   CCardText,
   CHeader,
-  CListGroup,
 } from '@coreui/react';
 // Import images
 import bryan from 'src/assets/images/bryan2.jpg';
@@ -22,10 +19,6 @@ const Dashboard = () => {
   // Array of room IDs and their names
   const rooms = [
     { id: 94, name: 'East Wing (05-76)' },
-    // { id: 180, name: 'Huddle A (05-44)' },
-    // { id: 163, name: 'Huddle B (05-67)' },
-    // { id: 1609, name: 'Huddle C (05-98)' },
-    // { id: 2189, name: 'Huddle Room D' },
     { id: 2194, name: 'Innovation Lab (05-92)' },
     { id: 139, name: 'Valencia (05-43)' },
     { id: 140, name: 'West Wing (05-20)' },
@@ -48,20 +41,20 @@ const Dashboard = () => {
         const seconds = ('0' + date.getSeconds()).slice(-2);
         return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
       };
-  
+
       // Set startDateTime to the current local time
       const startDateTime = formatDateTime(new Date());
-  
+
       // Set endDateTime to the end of the day (11:59:59 PM)
       const endOfDay = new Date();
       endOfDay.setHours(23, 59, 59, 999);
       const endDateTime = formatDateTime(endOfDay);
-  
+
       const allBookings = {}; // Object to store all the data
-  
+
       for (const room of rooms) {
         const roomId = room.id;
-  
+
         // Make a request to backend server
         const response = await fetch(
           `http://localhost:5001/api/bookings?startDateTime=${encodeURIComponent(
@@ -70,43 +63,42 @@ const Dashboard = () => {
             endDateTime
           )}&roomId=${encodeURIComponent(roomId)}`
         );
-  
+
         if (!response.ok) {
           console.error(`Network response was not ok for room ${roomId}`);
           continue; // Skip to next room
         }
-  
+
         const data = await response.json();
         const roomBookings = data.bookings || data;
-  
+
         if (roomBookings && roomBookings.length > 0) {
           // Add room name to each booking for identification
           roomBookings.forEach((booking) => {
             booking.roomName = room.name;
           });
-  
+
           allBookings[roomId] = roomBookings;
           console.log(`Bookings for room ${roomId}:`, roomBookings);
         } else {
           console.log(`No bookings for room ${roomId}`);
         }
       }
-  
+
       setBookings(allBookings); // Update bookings state
-  
+
       // Aggregate bookings into a single array
       const aggregated = Object.values(allBookings).flat();
-  
+
       // Sort the aggregated bookings by start time
       aggregated.sort((a, b) => new Date(a.timeFrom) - new Date(b.timeFrom));
-  
+
       // Update the aggregated bookings state
       setAggregatedBookings(aggregated);
     } catch (error) {
       console.error('Error fetching bookings:', error);
     }
   };
-  
 
   // Fetch bookings initially and refresh every hour
   useEffect(() => {
@@ -145,36 +137,24 @@ const Dashboard = () => {
     {
       src: bryan,
       title: 'Bryan M. Sastokas, Chief Information Technology Officer (CITO)',
-     // text: "Deputy Chief, CITO, Bryan M. Sastokas leads Metro's ITS organization and is dedicated to helping customers and business stakeholders address today’s increasingly complex software and hardware infrastructure challenges.",
     },
     {
       src: pat,
       title: 'Patrick Astredo, EO IT Business Applications',
-     // text: 'The IT Business Applications group is comprised of Transit Applications, Business Application Services, and Digital Strategy & Innovation, & Web App. Patrick encompasses the values of Metro as proven by his continued commitment to excellence.',
     },
     {
       src: medic,
       title: 'Medik Ghazikhanian, EO Center of Excellence (CoE)',
-    //  text: 'The ITS CoE is composed of Governance, Program Management Office (PMO), ITS Training, ITS Communications, Geospatial Business Intelligence (GBI), and ITS Budget and Admin. This department is headed by Medik Ghazikhanian.',
     },
     {
       src: tee,
       title: 'Vincent Tee, EO Enterprise Architecture & Technology Integration',
-    //  text: 'The Enterprise Architecture & Tech Integration group comprises IT Capacity Management, Network Engineering, IT Service Continuity - Database and Storage Management, Configuration & Data Center Management.',
     },
     // Aggregated bookings slide
     {
       type: 'aggregatedBookings',
       title: 'All Meeting Times',
     },
-    // Include the booking slides for individual rooms
-    //...bookingSlides,
-    // Remove the map slide since we're displaying it statically
-    // {
-    //   src: map,
-    //   type: 'image',
-    //   text: 'Map',
-    // },
   ];
 
   useEffect(() => {
@@ -182,7 +162,7 @@ const Dashboard = () => {
     let delay = 7000; // Default delay
 
     if (currentSlide.type === 'aggregatedBookings') {
-      delay = 55000; // 20 seconds for aggregated bookings slide
+      delay = 55000; // 55 seconds for aggregated bookings slide
     } else if (currentSlide.type === 'bookings') {
       delay = 15000; // 15 seconds for individual bookings slides
     }
@@ -268,6 +248,10 @@ const Dashboard = () => {
           font-size: 2rem;
         }
 
+        .bookings-list.small-font {
+          font-size: 1.5rem;
+        }
+
         .bookings-list li {
           margin-bottom: 20px;
         }
@@ -307,14 +291,14 @@ const Dashboard = () => {
 
       {/* Title Component */}
       <CCard>
-      <div className="title-container">
-        <h1 className="title-text">Welcome to the 5th Floor</h1>
-      </div>
+        <div className="title-container">
+          <h1 className="title-text">Welcome to the 5th Floor</h1>
+        </div>
 
-      {/* Static Map Image */}
-      <div className="static-map-container">
-        <img src={map} alt="Map" className="static-map-image" />
-      </div>
+        {/* Static Map Image */}
+        <div className="static-map-container">
+          <img src={map} alt="Map" className="static-map-image" />
+        </div>
       </CCard>
       {/* Carousel */}
       <div className="carousel-container">
@@ -332,7 +316,11 @@ const Dashboard = () => {
                   </CCardTitle>
                 </CHeader>
                 {aggregatedBookings.length > 0 ? (
-                  <ul className="bookings-list">
+                  <ul
+                    className={`bookings-list ${
+                      aggregatedBookings.length > 2 ? 'small-font' : ''
+                    }`}
+                  >
                     {aggregatedBookings.map((booking) => {
                       const startTime = new Date(booking.timeFrom);
                       const endTime = new Date(booking.timeTo);
@@ -363,7 +351,10 @@ const Dashboard = () => {
                   </ul>
                 ) : (
                   // Display the no meetings message when there are no bookings
-                  <p className="bookings-list" style={{ fontSize: '2rem', textAlign: 'center' }}>
+                  <p
+                    className="bookings-list"
+                    style={{ fontSize: '2rem', textAlign: 'center' }}
+                  >
                     No meetings booked in any rooms currently.
                   </p>
                 )}
@@ -387,7 +378,13 @@ const Dashboard = () => {
                     {slides[currentIndex].title}
                   </CCardTitle>
                 </CHeader>
-                <ul className="bookings-list">
+                <ul
+                  className={`bookings-list ${
+                    bookings[slides[currentIndex].roomId].length > 2
+                      ? 'small-font'
+                      : ''
+                  }`}
+                >
                   {bookings[slides[currentIndex].roomId].map((booking) => {
                     const startTime = new Date(booking.timeFrom);
                     const endTime = new Date(booking.timeTo);
@@ -458,7 +455,7 @@ const Dashboard = () => {
               alt="Slide"
               style={{
                 width: '100%',
-               }}
+              }}
             />
           )}
         </div>
